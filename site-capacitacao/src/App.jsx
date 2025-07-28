9fe4d16f2d39b013df3746bc7d391c2ad6699f89
ValleskaVS <149725128+ValleskaVS@users.noreply.github.com>
2025-07-28 00:59:27 -0300

    funcionando esse (eu acho)

---
+++ site-capacitacao/src/App.jsx
@@ -0,0 +1,11 @@
+import HeaderPrincipal from './components/headerPrincipal/HeaderPrincipal';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white dark:bg-black text-black dark:text-white transition-colors">
+      <HeaderPrincipal />
+    </div>
+  );
+}
+
+export default App;
--- site-capacitacao/tailwind.config.js
+++ site-capacitacao/tailwind.config.js
@@ -1,18 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: 'class',
+  content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2563EB', 
+      },
+    },
   },
   plugins: [],
 };
-
-module.exports = {
-  darkMode: 'class',
-  content: ['./index.html', './src/**/*.{js,jsx}'],
-  theme: { extend: {} },
-  plugins: [],
-}
